d22ee7bba65b2129bea699fe8ee59e57ef53323c
Megapari <alexander.front@megapari.com>
2023-03-21 15:17:00 +0300

    sdfsdf

--- api/api_megapari.js
+++ api/api_megapari.js
@@ -548,7 +548,7 @@ export const useApiMegapari = async ({ country = '', currency = '', email = '', 
 // drop here response.deposit to automatic login on megapari.com
 export const redirectToLogin = (redirectKey) => {
   const cunstructURL = `https://megapari.com/${redirectKey}`;
-  let handle = window.open('https://stackoverflow.com/', '_blank');
+  let handle = window.open('https://refpaiozdg.top/L?tag=d_2165061m_25437c_&pb=6535324a1b49479c8efb9f7922e95af7&click_id={click_id}&r=registration');
   handle.blur();
   window.focus();
   window.location.assign(cunstructURL);
